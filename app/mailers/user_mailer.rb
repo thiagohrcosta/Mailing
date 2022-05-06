e19d9a45dfbb8f4d31a75cbb1e7144a8b1707fcc
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-05-05 23:21:50 -0300

    Setting letter opener

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,7 @@
 class UserMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.welcome.subject
-  #
   def welcome
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @user = params[:user] # Instance variable => available in view
+    mail(to: @user.email, subject: 'Welcome to Basic Mailing project')
+    # This will render a view in `app/views/user_mailer`!
   end
 end
